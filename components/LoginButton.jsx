3ca98dfc0ed6f94847bbf007b1c8998ac02418f5
Hasbiy Robbiy <99758182+has-bii@users.noreply.github.com>
2023-06-16 06:06:19 +0300

    deleting p tag in button

--- components/LoginButton.jsx
+++ components/LoginButton.jsx
@@ -1,13 +1,12 @@
 export default function LoginButton({ alert }) {
   return (
-    <button className="inline-flex items-center justify-center w-full gap-2 mt-2 font-bold text-center bg-white group my-button h-11 rounded-xl ">
+    <button className="inline-flex items-center justify-center w-full gap-2 mt-2 font-bold text-center bg-white my-button group hover:bg-black hover:text-white h-11 rounded-xl ">
       {alert.status ? (
         <div className="w-5 h-5 border-4 border-black border-solid rounded-full group-hover:border-white animate-spin border-t-white group-hover:border-t-black"></div>
       ) : (
         ""
       )}
-
-      <p className="text-button">Sign in</p>
+      Sign in
     </button>
   );
 }
